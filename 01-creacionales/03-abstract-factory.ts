b8e038981648eaff1c1300eef5ff65ff9bed3887
Maxi Jurado <maxi.jurado.dp@gmail.com>
2025-11-13 22:22:25 -0300

    Abstract Factory

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -12,8 +12,97 @@
  * https://refactoring.guru/es/design-patterns/abstract-factory
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  *  El propósito del Abstract Factory es crear familias de objetos relacionados
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+interface Hamburguer {
+    prepare(): void;
+}
+
+interface Drink {
+    pour(): void;
+}
+
+class ChickenHamburguer implements Hamburguer {
+
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cpollo.', COLORS.yellow);
+    }
+
+}
+
+class BeefHamburguer implements Hamburguer {
+
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cbeef.', COLORS.red);
+    }
+
+}
+
+class Water implements Drink {
+
+    pour(): void {
+        console.log('Sirviendo %cagua.', COLORS.blue);
+    }
+
+}
+
+class Soda implements Drink {
+
+    pour(): void {
+        console.log('Sirviendo %csoda.', COLORS.green);
+    }
+
+}
+
+interface RestaurantFactory {
+    createHamburguer(): Hamburguer;
+    createDrink(): Drink;
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+
+  createHamburguer(): Hamburguer {
+    return new BeefHamburguer();
+  }
+  
+  createDrink(): Drink {
+    return new Soda();
+  }
+
+}
+
+class HealthyFoodRestaurantFactory implements RestaurantFactory {
+
+  createHamburguer(): Hamburguer {
+    return new ChickenHamburguer();
+  }
+
+  createDrink(): Drink {
+    return new Water();
+  }
+
+}
+
+function main( factory: RestaurantFactory ) {
+
+    const hamburguer = factory.createHamburguer();
+    const drink = factory.createDrink();
+
+    hamburguer.prepare();
+    drink.pour();
+
+}
+
+console.log('\n%cPedido del menu regular', COLORS.pink);
+
+main( new FastFoodRestaurantFactory() );
+
+console.log('\n\n\n%cPedido del menu saludable', COLORS.purple);
+
+main( new HealthyFoodRestaurantFactory() );
