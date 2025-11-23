cb18f24aa73a93963b168cef193a5044672b3933
Maxi Jurado <maxi.jurado.dp@gmail.com>
2025-11-22 21:47:38 -0300

    Composite

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,68 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+    showDetails( indent?: string ): void;
+}
+
+class FileComponent implements FileSystemComponent {
+    
+    private name: string;
+
+    constructor( name: string ) {
+        this.name = name;
+    }
+
+    showDetails( indent?: string ): void {
+        console.log(`${indent} - Archivo: ${this.name}`);
+    }
+}
+
+class FolderComponent implements FileSystemComponent {
+    private name: string;
+    private contents: FileSystemComponent[] = [];
+
+    constructor( name: string ) {
+        this.name = name;
+    }
+
+    add( component: FileSystemComponent ): void {
+        this.contents.push(component);
+    }
+
+    showDetails( indent: string = '' ): void {
+        console.log(`${indent} + Carpeta: ${this.name}`);
+        this.contents.forEach( component => component.showDetails( indent + ' '));
+    }
+}
+
+function main() {
+    const file1 = new FileComponent('file1.txt');
+    const file2 = new FileComponent('file2.txt');
+    const file3 = new FileComponent('file3.txt');
+    const file4 = new FileComponent('file4.txt');
+
+    const folder = new FolderComponent('folder');
+    folder.add(file1);
+    folder.add(file2);
+    
+    const folder2 = new FolderComponent('folder2');
+    folder2.add(file3);
+    
+    const folder3 = new FolderComponent('folder3');
+    folder3.add(file4);
+    folder2.add(folder3);
+
+    const folder5 = new FolderComponent('folder5');
+    folder2.add(folder5);
+    
+    const rootFolder = new FolderComponent('root');
+    rootFolder.add(folder);
+    rootFolder.add(folder2);
+
+    rootFolder.showDetails();
+    
+}
+
+main();
--- 02-estructurales/03.2-composite.ts
+++ 02-estructurales/03.2-composite.ts
@@ -45,18 +45,29 @@ class MenuItem implements MenuComponent {
 // Representa una categoría de menú que puede contener otros ítems o subcategorías.
 class MenuCategory implements MenuComponent {
   // TODO: Crear dos propiedades privadas: name y items
-  // Name sting y items arreglo de MenuComponent
-  // Name es recibida en el constructor, items se inicializa como un arreglo vacío
+  private name: string;
+  private items: MenuComponent[];
+
+
+  constructor(name: string) {
+    this.name = name;
+    this.items = [];
+  }
 
   //TODO: Sobrecarga de operadores - Item puede ser MenuComponent o un arreglo de MenuComponent
-  add(item: unknown): void {
+  add(item: MenuComponent | MenuComponent[]): void {
     // TODO: Implementar la sobrecarga de operadores
-    throw new Error('Method not implemented.');
+    if (Array.isArray(item)) {
+      this.items.push(...item);
+      return;
+    }
+    this.items.push(item);
   }
 
   showDetails(indent: string = ''): void {
     console.log(`%c${indent}+ ${this.name}`, COLORS.blue);
     // TODO: Implementar foreach
+    this.items.forEach(item => item.showDetails(indent + ' '));
   }
 }
 
