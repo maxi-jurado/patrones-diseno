ba9f4c660027d79ffad690da480e3baf8424696c
Maxi Jurado <maxi.jurado.dp@gmail.com>
2025-11-22 21:03:23 -0300

    Bridge

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,84 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+interface Abililty {
+    use(): void;
+}
+
+class SwordAttack implements Abililty {
+    use(): void {
+        console.log("Ataca con %cespada ferozmente", COLORS.red);
+    }
+}
+
+class AxeAttack implements Abililty {
+    use(): void {
+        console.log("Ataca con %chacha brutalmente", COLORS.yellow);
+    }
+}
+
+class MagicSpell implements Abililty {
+    use(): void {
+        console.log("Ataca con %chechizo mágico", COLORS.blue);
+    }
+}
+
+class FireballSpell implements Abililty {
+    use(): void {
+        console.log("Ataca con %cbola de fuego", COLORS.orange);
+    }
+}
+
+abstract class Character {
+    protected ability: Abililty;
+    
+    constructor( ability: Abililty ) {
+        this.ability = ability;
+    }
+
+    setAbility( ability: Abililty ): void {
+        this.ability = ability;
+    }
+
+    abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+    override performAbility(): void {
+        console.log("\nEl guerrero esta listo para luchar");
+        this.ability.use();
+    }
+}
+
+class Mage extends Character {
+    override performAbility(): void {
+        console.log("\nEl mago esta listo para castigar");
+        this.ability.use();
+    }
+}
+
+function main() {
+
+    const warrior = new Warrior(new SwordAttack());
+    warrior.performAbility();
+
+    // const axeAttack = new AxeAttack();
+    // warrior.setAbility(axeAttack);
+    // warrior.performAbility();
+
+    warrior.setAbility(new AxeAttack());
+    warrior.performAbility();
+    
+
+    const mage = new Mage( new MagicSpell() );
+    mage.performAbility();
+
+    mage.setAbility(new FireballSpell());
+    mage.performAbility();
+
+}
+
+main();
--- 02-estructurales/02.3-bridge.ts
+++ 02-estructurales/02.3-bridge.ts
@@ -9,3 +9,76 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+// 1. Interfaz NotificationChannel
+// Define el método `send`, que cada canal de comunicación implementará.
+interface NotificationChannel {
+  send(message: string): void;
+}
+
+// 2. Implementaciones de Canales de Comunicación
+
+class EmailChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando correo electrónico: ${message}`);
+  }
+}
+
+class SMSChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando SMS: ${message}`);
+  }
+}
+
+class PushNotificationChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando Push: ${message}`);
+  }
+}
+
+// 3. Clase Abstracta Notification
+// Define la propiedad `channel` y el método `notify`
+
+abstract class Notification {
+  // TODO: Definir la propiedad `channel` de tipo NotificationChannel
+  protected channels: NotificationChannel[];
+  
+  // TODO: Definir el constructor de la clase
+  constructor( channels: NotificationChannel[] ) {
+    this.channels = channels;
+  }
+  // TODO: Definir el método `notify` y `setChannel` (abstractos)
+  abstract notify( message: string ): void;
+  abstract addChannel( channel: NotificationChannel ): void;
+}
+
+class AlertNotification extends Notification {
+
+    override notify( message: string ): void {        
+        console.log('\n%cNotificacio de alerta', COLORS.red);
+        this.channels.forEach((channel) => channel.send(message));
+    }
+
+    override addChannel( channel: NotificationChannel ): void {
+        this.channels.push(channel);
+    }
+}
+
+function main() {
+  
+    const channels = [
+        new EmailChannel(),
+        new SMSChannel(),
+        new PushNotificationChannel(),
+    ];
+
+    const alerta = new AlertNotification(channels);
+    alerta.notify('Alerta de seguridad: Alguien en frente de la casa');
+
+    console.log('\n');
+
+}
+
+main();
