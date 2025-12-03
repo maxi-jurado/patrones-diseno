0d25574f27658883257dd9525fb4119f9eb10e94
Maxi Jurado <maxi.jurado.dp@gmail.com>
2025-12-02 21:56:05 -0300

    Facade

--- 02-estructurales/05-facade.ts
+++ 02-estructurales/05-facade.ts
@@ -11,3 +11,119 @@
  *
  * https://refactoring.guru/es/design-patterns/facade
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+class Projector {
+    turnOn() {
+        console.log('Proyector encendido');
+    }
+    
+    turnOff() {
+        console.log('Proyector apagado');
+    }
+}
+
+class SoundSystem {
+    on() {
+        console.log('Sistema de sonido encendido');
+    }
+    
+    off() {
+        console.log('Sistema de sonido apagado');
+    }
+}
+
+class VideoPlayer {
+    on() {
+        console.log('Reproductor de video encendido');
+    }
+    
+    play( movie: string ) {
+        console.log(`Reproductor de video reproduciendo %c${movie}`, COLORS.blue);
+    }
+
+    stop() {
+        console.log('Reproductor de video detenido');
+    }
+
+    off() {
+        console.log('Reproductor de video apagado');
+    }
+}
+
+class PopcornMaker {
+    poppingPopcorn() {
+        console.log('Haciendo palomitas');
+    }
+
+    turnOffPoppingPopcorn() {
+        console.log('Deteniendo las palomitas');
+    }
+}
+
+interface HomeTheaterFacadeOptions {
+    projector: Projector;
+    soundSystem: SoundSystem;
+    videoPlayer: VideoPlayer;
+    popcornMaker: PopcornMaker;
+}
+
+class HomeTheaterFacade {
+
+    private projector: Projector;
+    private soundSystem: SoundSystem;
+    private videoPlayer: VideoPlayer;
+    private popcornMaker: PopcornMaker;
+
+    constructor({
+        projector,
+        soundSystem,
+        videoPlayer,
+        popcornMaker
+    }: HomeTheaterFacadeOptions ) {
+        this.projector = projector;
+        this.soundSystem = soundSystem;
+        this.videoPlayer = videoPlayer;
+        this.popcornMaker = popcornMaker;
+    }
+
+    watchMovie( movie: string ): void {
+        console.log(`Preparando para ver el movie %c${movie}`, COLORS.blue);
+        this.popcornMaker.poppingPopcorn();
+        this.projector.turnOn();
+        this.soundSystem.on();
+        this.videoPlayer.on();
+        this.videoPlayer.play(movie);
+
+        console.log('%cDisfrute la pelicula', COLORS.blue);
+    }
+
+    endWatchingMovie(): void {
+        console.log('%cTerminando de ver la pelicula', COLORS.blue);
+        this.popcornMaker.turnOffPoppingPopcorn();
+        this.projector.turnOff();
+        this.soundSystem.off();
+        this.videoPlayer.stop();
+        this.videoPlayer.off();
+
+        console.log('%cSistema apagado', COLORS.blue);
+    }
+}
+
+function main () {
+
+    const homeTheaterFacade = new HomeTheaterFacade({
+        projector: new Projector(),
+        soundSystem: new SoundSystem(),
+        videoPlayer: new VideoPlayer(),
+        popcornMaker: new PopcornMaker()
+    });
+    
+    homeTheaterFacade.watchMovie('Pulp Fiction');
+
+    homeTheaterFacade.endWatchingMovie();
+
+}
+
+main();
--- 02-estructurales/05.2-facade.ts
+++ 02-estructurales/05.2-facade.ts
@@ -59,6 +59,9 @@ class Memory {
 
 class ComputerFacade {
   // TODO: Agregar los atributos necesarios CPU, Memory y HardDrive
+  private cpu: CPU = new CPU();
+  private memory: Memory = new Memory();
+  private hardDrive: HardDrive = new HardDrive();
 
   // TODO: Agregar el constructor para instanciar los atributos CPU, Memory y HardDrive
   constructor() {}
@@ -68,8 +71,11 @@ class ComputerFacade {
 
     // TODO: ejecutar las operaciones necesarias para encender la computadora
     // 1. Cargar el sistema operativo en la memoria - memory.load(0, hardDrive.read(0, 1024))
+    this.memory.load(0, this.hardDrive.read(0, 1024));
     // 2. Saltar a la posición de memoria 0 - cpu.jump(0)
+    this.cpu.jump(0);
     // 3. Ejecutar las instrucciones del CPU - cpu.execute()
+    this.cpu.execute();
 
     console.log('Computadora lista para usar.\n');
   }
@@ -80,8 +86,11 @@ class ComputerFacade {
 
     // TODO: ejecutar las operaciones necesarias para apagar la computadora
     // 1. Detener las operaciones del CPU - cpu.stopOperations()
+    this.cpu.stopOperations();
     // 2. Liberar la memoria - memory.free()
+    this.memory.free();
     // 3. Cerrar el disco duro - hardDrive.close()
+    this.hardDrive.close();
 
     console.log('Computadora apagada.\n');
   }
