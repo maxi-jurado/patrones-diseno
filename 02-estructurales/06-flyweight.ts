842ca29cdc2ae510ec79b00c3c57812f1f7c9330
Maxi Jurado <maxi.jurado.dp@gmail.com>
2025-12-04 20:25:29 -0300

    Flyweight

--- 02-estructurales/06-flyweight.ts
+++ 02-estructurales/06-flyweight.ts
@@ -8,3 +8,81 @@
  *
  * https://refactoring.guru/es/design-patterns/flyweight
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+interface Location {
+    display( coordinates: { x: number, y: number }): void;
+}
+
+// Flyweight
+class LocationIcon implements Location {
+    private type: string;
+    private iconImg: string;
+
+    constructor( type: string, iconImage: string ) {
+        this.type = type;
+        this.iconImg = iconImage;
+    }
+
+    display( coordinates: { x: number, y: number } ) {
+        console.log(
+            `Coords: ${ this.type } en ${ coordinates.x }, ${ coordinates.y } con ícono %c[${ this.iconImg }]`,
+            COLORS.green
+        )
+    }
+}
+
+// Flyweight Factory
+class LocationFactory {
+    private icons: Record< string, LocationIcon> = {};
+
+    getLocationIcon( type: string ) {
+        if ( !this.icons[type] ) {
+            console.log(`%cCreando icono para ${ type }`, COLORS.red);
+            const imgIcon = `imagen_de_${type.toLocaleLowerCase()}.png`;
+            this.icons[type] = new LocationIcon(type, imgIcon);
+        }
+
+        return this.icons[type];
+    }
+}
+
+class MapLocation {
+    private coordinates: { x: number, y: number };
+    private icon: LocationIcon;
+
+    constructor(
+        x: number,
+        y: number,
+        icon: LocationIcon
+    ) {
+        this.coordinates = { x, y };
+        this.icon = icon;
+    }
+
+    display() {
+        this.icon.display(this.coordinates);
+    }
+}
+
+function main() {
+    const factory = new LocationFactory();
+
+    const locations = [
+        new MapLocation(10, 20, factory.getLocationIcon('hospital')),
+        new MapLocation(30, 40, factory.getLocationIcon('hospital')),
+        new MapLocation(50, 60, factory.getLocationIcon('hospital')),
+
+        new MapLocation(20, 80, factory.getLocationIcon('parque')),
+        new MapLocation(60, 100, factory.getLocationIcon('parque')),
+        
+        new MapLocation(35, 45, factory.getLocationIcon('escuela')),
+    ];
+
+    locations.forEach(location => location.display());
+
+}
+
+main()
+
--- 02-estructurales/06.2-flyweight.ts
+++ 02-estructurales/06.2-flyweight.ts
@@ -39,14 +39,13 @@ class BulletTypeFactory {
   private bulletTypes: Record<string, BulletType> = {};
 
   getBulletType(name: string, damage: number, color: string): BulletType {
-    // TODO: Implementar un método para obtener un tipo de bala
-    // Si no existe el tipo de bala, crearlo y guardarlo en la lista de tipos de bala
-    // Si existe el tipo de bala, devolverlo
+    const key = `${name}-${damage}-${color}`;
+    if ( !this.bulletTypes[key] ) {
+        console.log(`%cCreando instancia de ${ key }`, COLORS.red);
+        this.bulletTypes[key] = new BulletType(name, damage, color);
+    }
 
-    // TODO: El key, debería de ser un identificador único para cada tipo de bala
-    // name-damage-color
-
-    throw new Error('Method not implemented.');
+    return this.bulletTypes[key];
   }
 }
 
